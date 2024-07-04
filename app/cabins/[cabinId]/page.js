a4c1155ab971b8346451831249028f5993d653f6
bikokwa <34458781+bikokwa@users.noreply.github.com>
2024-07-04 06:19:02 +0300

    generate static params

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -2,10 +2,19 @@ import { getCabin } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import { title } from "process";
+import { getCabins } from "../../_lib/data-service";
 
 export async function generateMetadata({ params }) {
   const { name } = await getCabin(params.cabinId);
   return { title: `Cabin ${name}` };
+}
+
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+
+  const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
+
+  return ids;
 }
 
 export default async function Page({ params }) {
