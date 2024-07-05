5a601ff6bd16fbe174630eb8fa484e1515147ff6
bikokwa <34458781+bikokwa@users.noreply.github.com>
2024-07-05 06:55:47 +0300

    client components in server components

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -3,6 +3,7 @@ import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import { title } from "process";
 import { getCabins } from "../../_lib/data-service";
+import TextExpander from "../../_components/TextExpander";
 
 export async function generateMetadata({ params }) {
   const { name } = await getCabin(params.cabinId);
@@ -38,7 +39,9 @@ export default async function Page({ params }) {
             Cabin {name}
           </h3>
 
-          <p className="text-lg text-primary-300 mb-10">{description}</p>
+          <p className="text-lg text-primary-300 mb-10">
+            <TextExpander>{description}</TextExpander>
+          </p>
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
