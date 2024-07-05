a0ad4f7369318f7d7ed5c6161badf6e6f869b8a9
bikokwa <34458781+bikokwa@users.noreply.github.com>
2024-07-05 16:21:04 +0300

    share state between client and server

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,12 +2,15 @@ import Spinner from "@/app/_components/Spinner";
 import { title } from "process";
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
+import Filter from "../_components/Filter";
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default async function Page() {
+export default async function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -22,8 +25,12 @@ export default async function Page() {
         Welcome to paradise.
       </p>
 
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
